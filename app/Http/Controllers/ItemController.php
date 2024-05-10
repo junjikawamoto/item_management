96230bbb012e52383f3b143336826dfcb1e39447
junjikawamoto <126031469+junjikawamoto@users.noreply.github.com>
2024-05-10 15:49:14 +0900

    itemController.php変更

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -39,6 +39,7 @@ class ItemController extends Controller
             // バリデーション
             $this->validate($request, [
                 'name' => 'required|max:100',
+                'detail' => 'required|max:100',
             ]);
 
             // 商品登録
